11d83b81e1354e83a7db97544a0204c814898491
Elissa-DI <elissadusabe@gmail.com>
2024-01-14 15:52:43 +0200

    new changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,12 @@ import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Acme Dashboard',
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
   description: 'The official Next.js Course Dashboard, built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  // metadataBase: new URL('https://next-learn-dashboard.ver cel.sh'),
 };
 
 export default function RootLayout({
